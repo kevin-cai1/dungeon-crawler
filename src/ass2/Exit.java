88071ba1e2acec5880b6ecc6a4670a8614b39985
Gordon Xie <z5160618@student.unsw.edu.au>
2018-09-11 12:02:07 +1000

    Created GameSystem class

--- src/ass2/Exit.java
+++ src/ass2/Exit.java
@@ -4,4 +4,8 @@ public class Exit extends Entity{
 	public Exit() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	//public WinStatus CheckWin() {
+		// if player is on the exit tile, return win condition
+	//}
 }
