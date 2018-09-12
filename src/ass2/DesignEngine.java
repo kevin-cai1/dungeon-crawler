b227e1a413bb0f6c4e003ad00d3fae2170445798
Gordon Xie <z5160618@student.unsw.edu.au>
2018-09-12 14:57:20 +1000

    updated win condition checking

--- src/ass2/DesignEngine.java
+++ src/ass2/DesignEngine.java
@@ -5,14 +5,23 @@ import java.util.ArrayList;
 public class DesignEngine {
 	private ArrayList<Entity> entityList;
 	private Map map;
+	private GameState gameState;
+	private boolean enemyWinCondition;
+	private boolean boulderWinCondition;
+	private boolean treasureWinCondition;
 	
 	public DesignEngine(int x, int y) {
 		this.entityList = new ArrayList<>();
 		this.map = new Map();
-		
+		this.enemyWinCondition = false;
+		this.boulderWinCondition = false;
+		this.treasureWinCondition = false;
 	}
 	
 	public void runDesignMode() {
+		gameState = GameState.Design;
+		setWinConditions(); // After designing the dungeon, determines what the win conditions are.
+		
 		
 	}
 	
@@ -36,7 +45,44 @@ public class DesignEngine {
 				return false; 		// cannot place entities on top of boulders or walls
 			}
 		}
-		
 		return true;
 	}
+	
+	private boolean exitWinCondition() {
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				for (Entity e : map.getMap()[i][j].getEntities()) { // look through every single entity
+					if (e instanceof Exit) { // count all the exits
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	private void setWinConditions() {
+		if (exitWinCondition()) {
+			this.enemyWinCondition = false;
+			this.boulderWinCondition = false;
+			this.treasureWinCondition = false;
+		} else {
+			for (int i = 0; i < 20; i++) {
+				for (int j = 0; j < 20; j++) {
+					for (Entity e : map.getMap()[i][j].getEntities()) { // look through every single entity
+						if (e instanceof Treasure) { // count all the exits
+							treasureWinCondition = true;
+						}
+						if (e instanceof Enemy) {
+							enemyWinCondition = true;
+						}
+						if (e instanceof FloorSwitch) {
+							boulderWinCondition = true;
+						}
+					}
+				}
+			}
+		}
+	}
+	
 }
