191058d7d0af8c0ada7a9709f00947d7b2d5d58b
Gordon Xie <z5160618@student.unsw.edu.au>
2018-09-12 13:48:54 +1000

    updated design engine

--- src/ass2/DesignEngine.java
+++ src/ass2/DesignEngine.java
@@ -17,9 +17,24 @@ public class DesignEngine {
 	}
 	
 	public void placeEntity(Entity entity, int x, int y) {
-		Tile[][] tile = map.getMap();
-		entityList.add(entity);
-		tile[x][y].addEntity(entity);
+		if (validatePlacement(entity, x, y)) {
+			Tile[][] tile = map.getMap();
+			entityList.add(entity);
+			tile[x][y].addEntity(entity);
+		}
 	}
 
+	public boolean validatePlacement(Entity entity, int x, int y) {
+		if (x > 19 || x < 0 || y > 19 || y < 0) {
+			return false;			// cannot place out of bounds
+		}
+		
+		for (Entity e : Tile[x][y]) {
+			if (e instanceof Obstacle) {
+				return false; 		// cannot place entities on top of boulders or walls
+			}
+		}
+		
+		return true;
+	}
 }
