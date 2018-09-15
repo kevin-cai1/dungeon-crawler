d860753effdae29397326b6e0b7ee7d807b3a45b
jun-lin1 <36977477+jun-lin1@users.noreply.github.com>
2018-09-15 20:25:37 +1000

    tried to make test

--- src/ass2/KillingEnemyTest.java
+++ src/ass2/KillingEnemyTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class PlayerInteractionTest {
+class KillingEnemyTest {
 	GameEngine game;
 	Map gameMap;
 	Tile[][] map;
@@ -31,18 +31,33 @@ class PlayerInteractionTest {
 	@Test
 	void testSwingSwordUp() {
 		//System.out.println("swing sword up");
-		Sword sword = new Sword(gameMap.genID());
+		gameMap.getPlayer().putInventory(new Sword(gameMap.genID()));
 		Map expectedGameMap = new Map(10);
-		Player player = new Player(gameMap.genID());
-		Tile t = gameMap.getTile(4, 4);
+		Tile t = expectedGameMap.getTile(4, 4);
+		player = new Player(expectedGameMap.genID());
 		t.addEntity(player);
-
+		expectedGameMap.getPlayer().putInventory(new Sword(expectedGameMap.genID()));
 		Hunter hunter = new Hunter(gameMap.genID());
 		t = gameMap.getTile(4, 3);
 		t.addEntity(hunter);
+		hunter = new Hunter(gameMap.genID());
+		t = gameMap.getTile(4, 2);
+		t.addEntity(hunter);
+		hunter = new Hunter(gameMap.genID());
+		t = gameMap.getTile(3, 3);
+		t.addEntity(hunter);
+		hunter = new Hunter(gameMap.genID());
+		t = gameMap.getTile(5, 3);
+		t.addEntity(hunter);
+		System.out.println("help");
+		game.swing(Direction.NORTH);
+		System.out.println(gameMap.getTile(4, 3).getEntities().size() + "okay" + expectedGameMap.getTile(4, 3).getEntities().size());
+		assertTrue(gameMap.getTile(4, 3).getEntities().size() == expectedGameMap.getTile(4, 3).getEntities().size());
+		//assertTrue(gameMap.getTile(4, 4).getEntities().size() ==  expectedGameMap.getTile(4, 4).getEntities().size());
+		//assertEquals(gameMap, expectedGameMap);
 		
 		
-		
+		/*
 		System.out.println("\n");
 		Tile playerLocation = gameMap.getPlayerLocation();
 		Tile expectedLocation = expectedGameMap.getPlayerLocation();
@@ -53,6 +68,7 @@ class PlayerInteractionTest {
 		playerLocation = gameMap.getPlayerLocation();
 		assertTrue(playerLocation.getX() == expectedLocation.getX());
 		assertTrue(playerLocation.getY() == expectedLocation.getY());
+		*/
 	}
 
 }
