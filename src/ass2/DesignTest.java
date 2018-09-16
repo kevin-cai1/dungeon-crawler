ae251f2d0bee71dfbde6b4cb6d65906b3cc48ce7
AndyLiu11037 <32506371+AndyLiu11037@users.noreply.github.com>
2018-09-16 21:18:40 +1000

    design engine tests

--- src/ass2/DesignTest.java
+++ src/ass2/DesignTest.java
@@ -6,39 +6,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DesignTest {
-
+	DesignEngine designEngine;
 	@BeforeEach
 	void setUp() throws Exception {
+		designEngine = new DesignEngine(20);
 	}
 
 	@Test
-	void testDesignEngine() {
-		fail("Not yet implemented");
+	void testValidPlaceEntity() {
+		Map map = designEngine.getMap();
+		Strategist strategist = new Strategist(map.genID());
+		designEngine.placeEntity(strategist, 5, 5);
+		Tile tile = map.getTile(5, 5);
+		assert(tile.getEntities().size() == 2);
 	}
-
 	@Test
-	void testRunDesignMode() {
-		fail("Not yet implemented");
+	void testInvalidPlaceEntity() {
+		Map map = designEngine.getMap();
+		Strategist strategist = new Strategist(map.genID());
+		Wall wall = new Wall(map.genID());
+		designEngine.placeEntity(wall, 5, 5);
+		designEngine.placeEntity(strategist, 5, 5);
+		Tile tile = map.getTile(5, 5);
+		assert(tile.getEntities().size() == 2);
 	}
-
-	@Test
-	void testPlaceEntity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testValidatePlacement() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSave() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testLoad() {
-		fail("Not yet implemented");
-	}
-
 }
