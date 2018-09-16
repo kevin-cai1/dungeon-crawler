3125b0870be2718a53e4a290e94b4f15b6e58fea
AndyLiu11037 <32506371+AndyLiu11037@users.noreply.github.com>
2018-09-16 21:01:13 +1000

    lol

---
+++ src/ass2/DesignTest.java
@@ -0,0 +1,44 @@
+package ass2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DesignTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testDesignEngine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRunDesignMode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPlaceEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValidatePlacement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLoad() {
+		fail("Not yet implemented");
+	}
+
+}
