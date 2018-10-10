22ee26dc81fd6791952fffc7dc4dd040a91db0d2
keVinDiesel <36939547+keVinDiesel@users.noreply.github.com>
2018-10-10 18:07:08 +1100

    UI map representation
    person on floor

---
+++ src/application/DesignController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class DesignController {
+	@FXML
+	private GridPane map;
+	
+	@FXML
+	public void initialize() {
+		
+	}
+}
