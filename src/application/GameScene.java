9189898c5b6cc64dc343fae2712a2dd3bff65818
keVinDiesel <36939547+keVinDiesel@users.noreply.github.com>
2018-10-11 21:05:59 +1100

    key handler done

--- src/application/GameScene.java
+++ src/application/GameScene.java
@@ -3,7 +3,7 @@ package application;
 import java.io.IOException;
 
 import ass2.*;
-
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
@@ -11,6 +11,7 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Screen;
@@ -31,7 +32,27 @@ public class GameScene {
 		
 	public void display() throws Exception {
 		s.setTitle(title);
-		s.setScene(generateGrid());
+		Scene scene = generateGrid();
+		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				switch (event.getCode()) {
+					case W:		System.out.println("move up");	break;
+					case S:		System.out.println("move down");break;
+					case D:		System.out.println("move right");break;
+					case A:		System.out.println("move left");break;
+					case ESCAPE:	try {
+						goHome();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} break;
+				default:
+					break;
+				}
+			}
+		});
+		s.setScene(scene);
 		s.setResizable(false);
 		s.sizeToScene();
 		Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
@@ -175,5 +196,9 @@ public class GameScene {
 		t.addEntity(new Coward(gameMap.genID()));
 		
 		return gameMap.getMap();
+	}
+	
+	private void goHome() throws Exception {
+		new MenuScene(s).display();
 	}
  }
