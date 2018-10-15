e32a27908aeaf93711d3eee5b8aa14c45cd33053
Gordon Xie <z5160618@student.unsw.edu.au>
2018-10-16 02:54:11 +1100

    still trying to implement state pattern

--- src/ass2/Lose.java
+++ src/ass2/Lose.java
@@ -2,9 +2,14 @@ package ass2;
 
 public class Lose implements GameStateInterface {
 
+	/**
+	 * returns true if player is dead
+	 */
 	@Override
 	public boolean checkState(GameEngine gameEngine) {
-		if (gameEngine.getGameMap())
+		if (gameEngine.checkPlayerStatus() == false) {
+			return true;
+		}
 		return false;
 	}
 
