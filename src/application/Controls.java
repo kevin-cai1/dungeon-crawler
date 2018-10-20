9b9d4a59ce862b600ed3c22193f0a52663a83c9a
keVinDiesel <36939547+keVinDiesel@users.noreply.github.com>
2018-10-20 21:19:00 +1100

    starting control mapping

---
+++ src/application/Controls.java
@@ -0,0 +1,81 @@
+package application;
+
+import javafx.scene.input.KeyCode;
+
+public class Controls {
+	private KeyCode upButton;
+	private KeyCode downButton;
+	private KeyCode leftButton;
+	private KeyCode rightButton;
+	private KeyCode dropBomb;
+	private KeyCode swordToggle;
+	private KeyCode arrowToggle;
+	
+	
+	public Controls() {
+		// TODO Auto-generated constructor stub
+		this.upButton = KeyCode.W;
+		this.downButton = KeyCode.S;
+		this.leftButton = KeyCode.A;
+		this.rightButton = KeyCode.D;
+		this.dropBomb = KeyCode.B;
+		this.swordToggle = KeyCode.L;
+		this.arrowToggle = KeyCode.K;
+	}
+	
+	public void setUpButton(KeyCode button) {
+		this.upButton = button;
+	}
+	
+	public void setDownButton(KeyCode button) {
+		this.downButton = button;
+	}
+	
+	public void setLeftButton(KeyCode button) {
+		this.leftButton = button;
+	}
+	
+	public void setRightButton(KeyCode button) {
+		this.rightButton = button;
+	}
+	
+	public void setBombButton(KeyCode button) {
+		this.dropBomb = button;
+	}
+	
+	public void setSwordButton(KeyCode button) {
+		this.swordToggle = button;
+	}
+	
+	public void setArrowButton(KeyCode button) {
+		this.arrowToggle = button;
+	}
+	
+	public KeyCode getUpButton() {
+		return this.upButton;
+	}
+	
+	public KeyCode getDownButton() {
+		return this.downButton;
+	}
+	
+	public KeyCode getLeftButton() {
+		return this.leftButton;
+	}
+	
+	public KeyCode getRightButton() {
+		return this.rightButton;
+	}
+	
+	public KeyCode getBombButton() {
+		return this.dropBomb;
+	}
+	
+	public KeyCode getSwordButton() {
+		return this.swordToggle;
+	}
+	
+	public KeyCode getArrowButton() {
+		return this.arrowToggle;
+	}
+}
